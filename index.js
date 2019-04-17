ce06c0f872e46f8d96f1d6efeb5b42dc471a7a5d
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-04-17 04:14:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ const AgeIsValid = Number.isInteger(currentAge)
 
 const randomNumber = Math.random() * 20
 
-const randomInteger = Math.floor(Math.random()) *
+const randomInteger = Math.floor(randomInteger)
